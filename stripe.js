b52e9de14d5050432dd5904839b28b56f3c8377a
The Social Robin <81664242+thesocialrobin@users.noreply.github.com>
2024-01-04 16:16:51 -0600

    Update stripe.js

--- stripe.js
+++ stripe.js
@@ -1,22 +1,20 @@
 import {fetch} from 'wix-fetch';
-
+import {getAPIKey} from 'backend/secrets.jsw';
 
 export async function stripeToken(cardObject, apiKeyPk) {
 
-      
-
-    //const mySecret = await getSecret("myApiKeyName");
-
     let values = encodeCard(cardObject);
+    let apiKey = await getAPIKey(apiKeyPk);
 
 	const response = await fetch("https://api.stripe.com/v1/tokens", {
 		method: 'post',
 		headers: {
 			"Content-Type": "application/x-www-form-urlencoded",
-			"Authorization": "Bearer " + apiKeyPk
+			"Authorization": "Bearer " + apiKey
 		},
 		body: values
 	});
+
     if (response.status >= 200 && response.status < 300) {
 	    const json = await response.json();
 	    return json;
@@ -36,4 +34,3 @@ function encodeCard(cardObject) {
 	}
 	return encoded.substr(0, encoded.length - 1);
 }
-
